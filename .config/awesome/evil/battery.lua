974d38272096cb92c35dac22c9f6903c46630ae7
hitori1403 <hitori1403@gmail.com>
2022-03-15 22:50:10 +0700

    fix battery widget

--- .config/awesome/evil/battery.lua
+++ .config/awesome/evil/battery.lua
@@ -1,40 +1,59 @@
-local awful		= require('awful')
-local beautiful = require('beautiful')
-local naughty	= require('naughty')
+-- Battery script inspired from elenapan
+-- Provides:
+-- evil::battery
+--		{int} percentage
+-- evil::charger
+--		{boolean} plugged
 
-local notify_critcal_battery = true
+local awful = require('awful')
+local naughty = require('naughty')
+local inspect = require('lib.inspect')
 
-local show_battery_warning = function ()
-	local notif
-	notif = naughty.notify({
-		title = '[ BATTERY ]',
-		text = 'Your battery is not daijobu :<<',
-		icon = beautiful.get_random_waifu(),
-		timeout = 15,
-		run = function ()
-			notify_critcal_battery = false
-			notif.die(naughty.notificationClosedReason.dismissedByUser)
-		end
-	})
-end
+local update_interval = 30
 
-local battery_update = function (status)
-	local charger = status:match('Adapter %d+: (.*)'):sub(1, -2)
-	local value = tonumber(status:match('(%d+)%%'))
-	local icon = beautiful.icons.battery
+-- Subcribe to power supply status changes with acpi_listen
+local charger_script = "sh -c 'acpi_listen | grep --line-buffered ac_adapter'"
 
-	if charger == 'on-line' then
-		icon = beautiful.icons.battery_charging
-		if not notify_critcal_battery then
-			notify_critcal_battery = true
-		end
-	elseif value < 10 and notify_critcal_battery then
-			show_battery_warning()
+-- First get battery file percentage
+-- If there are multiple, only get the first one
+-- TODO support multiple batteries
+awful.spawn.easy_async_with_shell("sh -c 'out=\"$(find /sys/class/power_supply/BAT?/capacity)\" && (echo $out | head -1) || false'", function (battery_file, _, __, exit_code)
+	-- No battery file found
+	
+	if exit_code ~= 0 then
+		return
+	end
+	
+	-- Periodically get battery info
+	awful.widget.watch('cat ' .. battery_file, update_interval, function (_, stdout)
+		awesome.emit_signal('evil::battery', tonumber(stdout))
+	end)
+end)
+
+-- First get charger file path
+awful.spawn.easy_async_with_shell("sh -c 'out=\"$(find /sys/class/power_supply/ADP?/online)\" && (echo $out | head -1) || false'", function (charger_file, _, __, exit_code)
+	-- No charger file found
+	if exit_code ~= 0 then
+		return
 	end
 
-	awesome.emit_signal('evil::battery', icon, value)
-end
+	-- Then initialize function that emits charger info
+	local emit_charger_info = function ()
+		awful.spawn.easy_async('cat ' .. charger_file, function (stdout)
+			awesome.emit_signal('evil::charger', tonumber(stdout) == 1)
+		end)
+	end
 
-awful.widget.watch('acpi -ab', 60, function (_, stdout)
-	battery_update(stdout)
+	-- Run once to initialize widget
+	emit_charger_info()
+
+	-- Kill old acpi_listen process
+	awful.spawn.easy_async('pkill -x acpi_listen', function ()
+		-- Update chager status with each line printed
+		awful.spawn.with_line_callback(charger_script, {
+			stdout = function (_)
+				emit_charger_info()
+			end
+		})
+	end)
 end)
--- .config/awesome/widgets/battery.lua
+++ .config/awesome/widgets/battery.lua
@@ -1,6 +1,7 @@
-local wibox = require("wibox")
-local dpi = require('beautiful.xresources').apply_dpi
-local gears = require('gears')
+local dpi		= require('beautiful.xresources').apply_dpi
+local wibox		= require("wibox")
+local gears		= require('gears')
+local beautiful = require('beautiful')
 
 local icon_widget = wibox.widget {
 	fixed_height = 15,
@@ -9,6 +10,7 @@ local icon_widget = wibox.widget {
 }
 
 local value_widget = wibox.widget {
+	text = 'abc',
 	widget = wibox.widget.textbox
 }
 
@@ -24,9 +26,18 @@ local battery_widget = {
 	layout = wibox.layout.fixed.horizontal
 }
 
-awesome.connect_signal('evil::battery', function(icon, value)
+awesome.connect_signal('evil::battery', function (battery)
+	value_widget.text = battery .. '%'
+end)
+
+awesome.connect_signal('evil::charger', function (plugged)
+	local icon
+	if plugged then
+		icon = beautiful.icons.battery_charging
+	else
+		icon = beautiful.icons.battery
+	end
 	icon_widget.image = gears.color.recolor_image(icon, '#61c766')
-	value_widget.text = value .. '%'
 end)
 
 return battery_widget
