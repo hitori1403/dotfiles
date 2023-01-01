c34aecfb398ac7f170df46bc592aa4eaae45cceb
hitori1403 <hitori1403@gmail.com>
2023-01-01 17:39:44 +0700

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-check_root() {
-	if [[ $UID != 0 ]]; then
-    echo "Please run this script with sudo:"
-    echo "sudo $0 $*"
-    exit 1
-	fi
-}
-
 handle_acpi_events() {
 	FILE=/etc/systemd/logind.conf
 
+	echo "Modifying $FILE"
+
 	# Suspend when power key is pressed
-	sed -i '/HandlePowerKey=/s/poweroff/suspend/g' $FILE
-	sed -i '/HandlePowerKey=/s/^#//g' $FILE
+	sudo sed -i '/#HandlePowerKey=/s/poweroff/suspend/g' $FILE
+	sudo sed -i '/#HandlePowerKey=/s/^#//g' $FILE
 }
 
 adjust_touchpad_rules() {
-	FILE=.sysconf/etc/udev/rules.d/01-touchpad.rules
-	sed -i "s/user/$USER/g" $FILE
-	sed -i "s|address|$DBUS_SESSION_BUS_ADDRESS|g" $FILE
+	FILE=/etc/udev/rules.d/01-touchpad.rules
+	
+	echo "Modifying $FILE"
+
+	sudo sed -i "s/user/$USER/g" $FILE
+	sudo sed -i "s|address|$DBUS_SESSION_BUS_ADDRESS|g" $FILE
 }
 
 copy_config() {
-	cp -r .sysconf/* /etc
-	su $USER -c 'cp -r .[^.]* ~ && mv ~/.git ~/.dotfiles'
+	echo "Copying configs"
+	sudo cp -r .sysconf/* /
+	cp -r .[^.]* ~ && mv ~/.git ~/.dotfiles
 }
 
-check_root()
-handle_acpi_events()
-adjust_touchpad_rules()
-copy_config()
+copy_config
+
+handle_acpi_events
+adjust_touchpad_rules
