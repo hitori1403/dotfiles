0ba34b39524951df265847d995a52d16925571f1
hitori1403 <hitori1403@gmail.com>
2024-01-10 08:22:54 +0700

    add fidget and migrate from rust-tools to rustaceanvim

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -132,21 +132,8 @@ return {
 		lazy = true
 	},
 
-	-- Rust LSP
 	{
-		'simrat39/rust-tools.nvim',
+		'mrcjkb/rustaceanvim',
 		ft = { 'rust' },
-		opts = {
-			server = {
-				on_attach = function(_, bufnr)
-					local rt = require("rust-tools")
-					-- Hover actions
-					vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-					-- Code action groups
-					vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-				end,
-			}
-		},
-		dependencies = { 'neovim/nvim-lspconfig' },
-	}
+	},
 }
