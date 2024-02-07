0725f80803ea478841e0b4251745cadd81d047bf
hitori1403 <hitori1403@gmail.com>
2024-02-07 09:13:23 +0700

    handle C/C++ keybinding

--- .config/nvim/ftplugin/c.lua
+++ .config/nvim/ftplugin/c.lua
@@ -1,6 +1,12 @@
 local toggleterm = require('toggleterm')
 
+if vim.bo.filetype == 'c' then
+	cmd = 'gcc -Wall % -o %:r && ./%:r'
+else
+	cmd = 'g++ -Wall % -o %:r && ./%:r'
+end
+
 vim.keymap.set('n', '<F9>', function()
 	vim.cmd.write()
-	toggleterm.exec(vim.fn.expandcmd('gcc -Wall % -o %:r && ./%:r'))
+	toggleterm.exec(vim.fn.expandcmd(cmd))
 end)
--- .config/nvim/ftplugin/cpp.lua
+++
@@ -1,6 +0,0 @@
-local toggleterm = require('toggleterm')
-
-vim.keymap.set('n', '<F9>', function()
-	vim.cmd.write()
-	toggleterm.exec(vim.fn.expandcmd('g++ -Wall % -o %:r && ./%:r'))
-end)
