143f8caef56d53d30768d14d4016dc4d16513f72
hitori1403 <hitori1403@gmail.com>
2024-02-17 00:26:14 +0700

    update cmp config

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,12 +1,18 @@
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",                              -- LSP source for nvim-cmp
-		{ "saadparwaiz1/cmp_luasnip", dependencies = "nvim-cmp" }, -- Snippets source for nvim-cmp
+		"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+		"saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
 		"L3MON4D3/LuaSnip",
-		"hrsh7th/cmp-path"                                   -- Path completion
+		"hrsh7th/cmp-path"    -- Path completion
 	},
 	config = function()
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
 		local luasnip = require("luasnip")
 		local lspkind = require('lspkind')
 
@@ -18,22 +24,27 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}),
+				['<C-b>'] = cmp.mapping.scroll_docs(-4),
+				['<C-f>'] = cmp.mapping.scroll_docs(4),
+				['<C-Space>'] = cmp.mapping.complete(),
+				['<C-e>'] = cmp.mapping.abort(),
+				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+				-- luasnip mapping
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
+						-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+						-- that way you will only jump inside the snippet region
 					elseif luasnip.expand_or_jumpable() then
 						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
 					else
 						fallback()
 					end
 				end, { "i", "s" }),
+
 				["<S-Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_prev_item()
@@ -44,11 +55,13 @@ return {
 					end
 				end, { "i", "s" }),
 			}),
-			sources = {
+			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
 				{ name = "path" }
-			},
+			}, {
+				{ name = 'buffer' }
+			}),
 			formatting = {
 				format = lspkind.cmp_format({
 					mode = 'symbol_text', -- show only symbol annotations
